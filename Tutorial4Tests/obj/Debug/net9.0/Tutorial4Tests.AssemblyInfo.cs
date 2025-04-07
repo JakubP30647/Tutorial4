c2cced0f55eaa6514909865d301a28079a11dd89
JakubP30647 <s30647@pjwstk.edu.pl>
2025-04-07 20:47:11 +0200

    AAAAAAAAAAAAAAAAAAAA

--- Tutorial4Tests/obj/Debug/net9.0/Tutorial4Tests.AssemblyInfo.cs
+++ Tutorial4Tests/obj/Debug/net9.0/Tutorial4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429f7dd46795e2e527eabfc38a8af6a530aaa8bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c20984c8bcf7072627904934e04da902404053a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
